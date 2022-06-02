9d44f8559686947e8b2d87f7a810d780f354e5b1
Mitchell Freeman <mitchell.w.freeman13@gmail.com>
2022-06-01 22:40:02 -0400

    routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,60 +1,63 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
+    const newPost = await Post.create({ 
+      ...req.body,
+      user_id: req.session.user.id
     });
+      res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.post('/login', async (req, res) => {
+router.put('/l:id', withAuth, async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
+    const updatePost = await Post.update({
+       where: {
+        id: req.params.id,
+        user_id: req.session.user_id
+      },
+      ...req.body,
+      user_id: req.session.user_id
     });
-
+      res.status(200).json(updatePost);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
+router.delete('/l:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id
+      }
     });
-  } else {
-    res.status(404).end();
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!'});
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.post('/:id', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      body: req.body.newComment,
+      post_id: req.params.id,
+      user_id: req.session.user_id
+    });
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
         include: [
           {
             model: User,
-            attributes: ['username'],
+            attributes: ['id','username'],
           },
         ],
       });
@@ -24,14 +24,49 @@ router.get('/', async (req, res) => {
     }
   });
 
-  router.get('/posts/:id', async (req, res) => {
+router.get('/dashboard'. withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  }catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('login');
+});
+
+router.get('/post/:id', async (req, res) => {
     try {
       const postData = await Post.findByPk(req.params.id, {
         include: [
           {
             model: User,
-            attributes: ['username'],
+            attributes: ['id','username'],
           },
+          {
+            model: Comment,
+            attributes: ['body', 'id', 'user_id', 'post_id', 'date_submitted'],
+            include: [
+              {
+                model: User,
+                attributes: ['id', 'username']
+              }
+            ]
+          }
         ],
       });
   
